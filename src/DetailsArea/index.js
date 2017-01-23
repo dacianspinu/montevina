3a2be9f910ee581012f20c22fda2705cfc60e59d
Dacian Spînu <macbook@Macs-MacBook-Air.local>
2017-01-24 00:22:27 +0200

    DetailsArea component

---
+++ src/DetailsArea/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class DetailsArea extends Component {
+  render() {
+    return (
+      <div className="big-area">Details Area</div>
+    );
+  }
+}
+
+export default DetailsArea;
