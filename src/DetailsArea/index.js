eaa78bdead272429f6bb3f20569704051d5f0f1f
Dacian Spînu <macbook@Macs-MacBook-Air.local>
2017-01-24 18:32:21 +0200

    Added titles for boxes.

--- src/DetailsArea/index.js
+++ src/DetailsArea/index.js
@@ -6,11 +6,17 @@ class DetailsArea extends Component {
     return (
       <div className="big-area big-area-right">
         <div className="details-box projects">
-          
+          <i className="em em-computer"></i><h1 className="box-title">Projects</h1>
         </div>
-        <div className="details-box experience"></div>
-        <div className="details-box studies"></div>
-        <div className="details-box photo"></div>  
+        <div className="details-box experience">
+          <i className="em em-briefcase"></i><h1 className="box-title">Experience</h1>
+        </div>
+        <div className="details-box studies">
+          <i className="em em-books"></i><h1 className="box-title">Studies</h1>
+        </div>
+        <div className="details-box photo">
+          <i className="em em-camera"></i><h1 className="box-title">Photos</h1>
+        </div>  
       </div>
     );
   }
