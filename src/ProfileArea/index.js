b68181b25f260b9e9bbf59cf722603cf3d0de802
Dacian Spînu <macbook@Macs-MacBook-Air.local>
2017-01-24 14:51:49 +0200

    Completed profile area.

--- src/ProfileArea/index.js
+++ src/ProfileArea/index.js
@@ -5,10 +5,23 @@ import './index.css'
 
 class ProfileArea extends Component {
   render() {
+
+    let backgroundImageStyle = {
+        backgroundImage: "url(" + process.env.PUBLIC_URL + '/profile.jpg)'
+    }
+
     return (
-      <div className="big-area vertical-centered-content horizontal-centered-content">
+      <div className="big-area big-area-left vertical-centered-content horizontal-centered-content">
         <div className="vertical-centered-content-inner-wrapper">
-            <div className="profile-picture"></div>
+            <div className="details-wrapper">
+                <div className="profile-picture" style={backgroundImageStyle}></div>
+                <h2 className="profile-keyword">Front-End Developer.</h2>
+                <h2 className="profile-keyword">Dreamer &</h2>
+                <h2 className="profile-keyword">Geek.</h2>
+
+
+                <p className="profile-description"> Hi there! I am Dacian Spînu and I am currently developing awesome front-end apps (@)Maxcode in Iasi, Romania.</p>
+            </div>
         </div>
       </div>
     );
