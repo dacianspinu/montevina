26cafa2d0b478995df42edea490dee693a58c409
Dacian Spînu <macbook@Macs-MacBook-Air.local>
2017-01-24 14:52:19 +0200

    Started work on details area, trying some color combinations.

--- src/DetailsArea/index.js
+++ src/DetailsArea/index.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import "./index.css"
 
 class DetailsArea extends Component {
   render() {
     return (
-      <div className="big-area">Details Area</div>
+      <div className="big-area big-area-right">
+        <div className="details-box projects">
+          
+        </div>
+        <div className="details-box experience"></div>
+        <div className="details-box studies"></div>
+        <div className="details-box photo"></div>  
+      </div>
     );
   }
 }
