72cfb8137fa35eb8925e1317a70eeb60ccc99430
Dacian Spinu <dacian@Dacians-Mac-mini.local>
2017-01-25 11:18:13 +0200

    Fixes for image position.

--- src/DetailsArea/index.js
+++ src/DetailsArea/index.js
@@ -10,16 +10,22 @@ class DetailsArea extends Component {
             <i className="em em-computer"></i><h1 className="box-title">Projects</h1>
           </div>
 
-          <img src={ process.env.PUBLIC_URL + 'stackla_project.png' } className="box-image computer"></img>
+          <img src={ process.env.PUBLIC_URL + 'stackla_project.png' } className="box-image computer" role="presentation"></img>
         </div>
         <div className="details-box experience">
-          <i className="em em-briefcase"></i><h1 className="box-title">Experience</h1>
+          <div className="box-item-wrapper title">
+            <i className="em em-briefcase"></i><h1 className="box-title">Experience</h1>
+          </div>
         </div>
         <div className="details-box studies">
-          <i className="em em-books"></i><h1 className="box-title">Studies</h1>
+          <div className="box-item-wrapper title">
+            <i className="em em-books"></i><h1 className="box-title">Studies</h1>
+          </div>
         </div>
         <div className="details-box photo">
-          <i className="em em-camera"></i><h1 className="box-title">Photos</h1>
+          <div className="box-item-wrapper title">
+            <i className="em em-camera"></i><h1 className="box-title">Photos</h1>
+          </div>
         </div>  
       </div>
     );
